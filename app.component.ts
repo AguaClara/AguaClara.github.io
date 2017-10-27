a89b5792ce37ddea6c42fa3bf8efb96c17ec2fd7
GPacenza <31751381+GPacenza@users.noreply.github.com>
2017-10-27 17:15:35 -0400

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'my-app',
+
+  styles: [`
+        h1 {
+    color:#545454;
+    background:#87CEFA;
+    padding:15px;
+    box-shadow:2px 2px 2px 0 rgba(0, 0, 0, .3);
+    }
+    `]
+
+  template: `<h1>{{title}}</h1>`,
+})
+
+export class AppComponent  {
+  title= 'App Component'
+  }

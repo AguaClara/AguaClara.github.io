0902662df0bdaa5d86ca1086955af4d0849f583f
GPacenza <arlo.pacenza@gmail.com>
2017-11-11 16:27:45 -0500

    data service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+
+  styles: [`
+        h1 {
+    color:#545454;
+    background:#87CEFA;
+    padding:15px;
+    box-shadow:2px 2px 2px 0 rgba(0, 0, 0, .3);
+    }
+    `],
+
+  template: `<h1>{{title}}</h1>`,
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  {
+  title= 'App Component'
+  }
